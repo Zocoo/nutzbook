a4c9bec5668354f73dd5ca5b11ebf5638625d747
吴易泽 <815769472@qq.com>
2015-12-14 17:52:13 +0800

    xx

---
+++ src/net/wendal/nutzbook/MainSetup.java
@@ -0,0 +1,34 @@
+package net.wendal.nutzbook;
+
+import java.util.Date;
+
+import net.wendal.nutzbook.bean.User;
+
+import org.nutz.dao.Dao;
+import org.nutz.dao.util.Daos;
+import org.nutz.ioc.Ioc;
+import org.nutz.mvc.NutConfig;
+import org.nutz.mvc.Setup;
+
+public class MainSetup implements Setup {
+
+	public void init(NutConfig conf) {
+		Ioc ioc = conf.getIoc();
+		Dao dao = ioc.get(Dao.class);
+		Daos.createTablesInPackage(dao, "net.wendal.nutzbook", false);
+
+		// 初始化默认根用户
+		if (dao.count(User.class) == 0) {
+			User user = new User(); 
+			user.setName("admin");
+			user.setPassword("123456");
+			user.setCreateTime(new Date());
+			user.setUpdateTime(new Date());
+			dao.insert(user);
+		}
+	}
+
+	public void destroy(NutConfig conf) {
+	}
+
+}
