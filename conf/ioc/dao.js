801f8100f1bcf78775c6242a0294fd59e6c1c6ab
zocoo <815769472@qq.com>
2015-12-15 09:48:00 +0800

    xx

--- conf/ioc/dao.js
+++ conf/ioc/dao.js
@@ -8,7 +8,7 @@ var ioc = {
 		fields : {
 			url : "jdbc:mysql://127.0.0.1:3306/nutzbook",
 			username : "wuyize",
-			password : "123456",
+			password : "psoxsk",
 			testWhileIdle : true,
 			validationQuery : "select 1",
 			maxActive : 100
